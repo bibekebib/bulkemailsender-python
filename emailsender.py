11ffd7c09acbd8ed00e69666d600ab060a25f4cd
bibekebib <42284727+bibekebib@users.noreply.github.com>
2020-05-23 14:19:06 +0545

    alpha-release1

--- emailsender.py
+++ emailsender.py
@@ -1,24 +1,27 @@
 from smtplib import SMTP
 import csv
-gmail = 'example@example.com'
-password = '*************'
-message = open('message.txt',mode='r').read() 
-
-with open ('ok - Sheet1.csv') as file:
+gmail = 'admin@example.com'
+password = 'yourpassword'
+message = open('message.txt', mode='r').read()
+message1 = str(message)
+with open('ok - Sheet1.csv') as file:
     reader = csv.reader(file)
     next(reader)
 
     for Name, Email in reader:
 
-            mailto = Email
-            msg = ('Dear ' + Name +'\n'+ message).encode('utf-8')
-            mailServer = SMTP('smtp.gmail.com', 587)
-            mailServer.starttls()
-            mailServer.login(gmail, password)
-            print('sending email to'+mailto+'\n')
-            mailServer.sendmail(gmail, mailto, msg)
-            
-            print('\n successfully sent to '+mailto)
-            
-            mailServer.quit()
+        mailto = Email
+        subject = 'This is testing mail.'
+        msg1 = 'Dear ' + Name + '\n' + message1
+        msg = 'subject: {}\n\n {}'.format(subject, msg1)
+        mailServer = SMTP('smtp.gmail.com', 587)
+        mailServer.starttls()
+        mailServer.login(gmail, password)
+        print('sending email to'+mailto+'\n')
+        mailServer.sendmail(gmail, mailto, msg.encode('utf-8'))
+
+        print('\n successfully sent to '+mailto)
+
+        mailServer.quit()
 file.close()
+
